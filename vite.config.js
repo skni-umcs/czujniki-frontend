0545d03c08b31aa3e42bce06d4fe21511cd0fc6d
maciej.bednarczyk <maciej.bednarczyk@skni.umcs.pl>
2024-09-30 18:08:51 +0200

    Make css classnames human readable in development

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 // @ts-check
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(env => ({
     plugins: [react()],
-});
+    css: {
+        modules: {
+            generateScopedName: env.mode == "development"
+                ? (className, filePath) => {
+                        const fileName = path.basename(filePath, ".module.css");
+                        return `${fileName}__${className}`;
+                    }
+                : undefined,
+        },
+    },
+}));
