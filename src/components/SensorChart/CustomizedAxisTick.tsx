84cb428bfa9e0ffcf976f0fd82cf040c9d4b2d7c
maciej.bednarczyk <maciej.bednarczyk@skni.umcs.pl>
2025-03-14 16:49:14 +0100

    Format time using RelativeTimeFormat and DateTimeFormat in CustomizedAxisTick

--- src/components/SensorChart/CustomizedAxisTick.tsx
+++ src/components/SensorChart/CustomizedAxisTick.tsx
@@ -6,19 +6,26 @@ interface ITickProps {
     };
 }
 
+const relativeDayFormatter = new Intl.RelativeTimeFormat("pl", { numeric: "auto" });
+const timeFormatter = new Intl.DateTimeFormat("pl", { timeStyle: "medium" });
+const dateFormatter = new Intl.DateTimeFormat("pl");
+
 const CustomizedAxisTick = ({ x, y, payload }: ITickProps) => {
-    const date = new Date(payload.value);
+    const payloadDate = new Date(payload.value);
     const now = new Date();
 
-    let secondRow = date.toLocaleString().split(", ")[0];
+    const todaysDayMs = new Date(now.toDateString()).getTime();
+    const payloadDayMs = new Date(payloadDate.toDateString()).getTime();
 
-    if (date.getDate() === now.getDate() - 1) secondRow = "wczoraj";
-    else if (date.getDate() === now.getDate()) secondRow = "dzisiaj";
+    const diffDays = Math.trunc((payloadDayMs - todaysDayMs) / (1000 * 60 * 60 * 24));
+    const secondRow = diffDays >= -2
+        ? relativeDayFormatter.format(diffDays, "day")
+        : dateFormatter.format(payloadDate);
 
     return (
         <g transform={`translate(${x.toString()},${y.toString()})`} fontSize="0.8em">
             <text dy={16} textAnchor="end" fill="currentColor" transform="rotate(-35)">
-                {date.toLocaleString().split(", ")[1]}
+                {timeFormatter.format(payloadDate)}
             </text>
             <text dy={32} textAnchor="end" fill="currentColor" transform="rotate(-35)">
                 {secondRow}
