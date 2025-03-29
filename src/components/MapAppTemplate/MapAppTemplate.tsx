b9996c4b4aafb470896e9bde273fed5993520530
maciej.bednarczyk <maciej.bednarczyk@skni.umcs.pl>
2025-03-29 16:45:33 +0100

    Remove AppHeader from mobile

--- src/components/MapAppTemplate/MapAppTemplate.tsx
+++ src/components/MapAppTemplate/MapAppTemplate.tsx
@@ -1,12 +1,10 @@
 import styles from "./MapAppTemplate.module.css";
 import Navbar from "../Navbar/Navbar";
-import AppHeader from "../AppHeader/AppHeader";
 import MniswBar from "../MniswBar/MniswBar";
 
 const MainAppTemplate: React.FC<React.PropsWithChildren> = ({ children }) => {
     return (
         <>
-            <AppHeader className={styles.appHeader} />
             <MniswBar />
             <div className={styles.wrapper}>
                 <Navbar />
