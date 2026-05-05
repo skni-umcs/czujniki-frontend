01ea3fb03ab483e70f37046c5bd92b91a7aaa32a
maciej.bednarczyk <maciej.bednarczyk@skni.umcs.pl>
2026-05-05 21:05:28 +0200

    upgrade dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 // @ts-check
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
 import reactPlugin from "eslint-plugin-react";
+import reactRefreshPlugin from "eslint-plugin-react-refresh";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import StylisticPlugin from "@stylistic/eslint-plugin";
 
@@ -20,13 +22,14 @@ export default tseslint.config(
     ...tseslint.configs.strictTypeChecked,
     ...tseslint.configs.stylisticTypeChecked,
     StylisticPlugin.configs["disable-legacy"],
-    reactPlugin.configs.flat.recommended,
-    reactPlugin.configs.flat["jsx-runtime"],
-    jsxA11y.flatConfigs.recommended,
+    fixupConfigRules(reactPlugin.configs.flat.recommended),
+    fixupConfigRules(reactPlugin.configs.flat["jsx-runtime"]),
+    fixupConfigRules(jsxA11y.flatConfigs.recommended),
     {
         plugins: {
-            "react": reactPlugin,
+            "react": fixupPluginRules(reactPlugin),
             "react-hooks": reactHooksPlugin,
+            "react-refresh": reactRefreshPlugin,
             "@stylistic": StylisticPlugin,
         },
         rules: {
@@ -34,6 +37,7 @@ export default tseslint.config(
             "react/prop-types": 0,
             "react-hooks/rules-of-hooks": "warn",
             "react-hooks/exhaustive-deps": "warn",
+            "react-refresh/only-export-components": "warn",
             "@stylistic/jsx-one-expression-per-line": 0,
             "@typescript-eslint/restrict-template-expressions": ["error", {
                 allowNumber: true,
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { defineConfig } from "vite";
 import path from "path";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(env => ({
